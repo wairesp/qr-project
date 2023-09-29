c4e33ad086f630fae8d2b139fb7072d10b434172
wairesp <54559794+wairesp@users.noreply.github.com>
2023-09-29 00:55:00 -0500

    added qr scanner from npm

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  ScannerQRCodeConfig,
+  ScannerQRCodeResult,
+  NgxScannerQrcodeService,
+  NgxScannerQrcodeComponent,
+  ScannerQRCodeSelectedFiles,
+} from 'ngx-scanner-qrcode';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "im gonna woop yo ass";
+
+export class AppComponent implements AfterViewInit {
+  title = "DieLess";
+  
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#front_and_back_camera
+  public config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        width: window.innerWidth,
+        height: window.innerHeight
+      },
+    },
+   
+    canvasStyles: [
+      { /* layer */
+        lineWidth: 1,
+        /*85 represents transparency*/
+        fillStyle: '#00949485',
+        strokeStyle: '#00949485',
+      },
+      { /* text */
+        font: '17px serif',
+        fillStyle: '#ffffff',
+        strokeStyle: '#ffffff',
+      }
+    ],
+    isBeep: false,
+    fps:30,
+    src:'../assets/cat1.jpg'
+  };
+  public isScanned = false;
+  //holds results
+  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
+  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
+
+  //reference to the component
+  @ViewChild('action') action!: NgxScannerQrcodeComponent;
+
+  //injects the service
+  constructor(private qrcode: NgxScannerQrcodeService) { }
+
+  //coment the handle to start automatically
+  ngAfterViewInit(): void {
+    this.action.isReady.subscribe((res: any) => {
+       this.handle(this.action, 'start');
+    });
+  }
+
+  //when a event is emited to the scanner logs it into the console
+  //the line above console pauses once it gets a qr
+  public onEvent(e: ScannerQRCodeResult[], action?: any): void {
+    e && action && action.pause();
+    console.log(e);
+  }
+
+  //USED TO START STOP PLAY OR PAUSE THE SCANNER
+  public handle(action: any, fn: string): void {
+    const playDeviceFacingBack = (devices: any[]) => {
+      // front camera or back camera check here!
+      const device = devices.find(f => (/back|rear|environment/gi.test(f.label))); // Default Back Facing Camera
+      action.playDevice(device ? device.deviceId : devices[0].deviceId);
+    }
+
+    if (fn === 'start') {
+      action[fn](playDeviceFacingBack).subscribe((r: any) => console.log(fn, r), alert);
+    } else {
+      action[fn]().subscribe((r: any) => console.log(fn, r), alert);
+    }
+  }
+
+  //merged method
+  public handleStartOrUnpause(action: any): void {
+    if (!action.isStart) {
+      // If the scanner is not started, start it
+      this.handle(action, 'start');
+    } else if (action.isPause) {
+      // If the scanner is started but paused, unpause it
+      this.handle(action, 'play');
+    }
+  }
+
+  //to upload files meh
+  public onSelects(files: any) {
+    this.qrcode.loadFiles(files, 0.5).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      this.qrCodeResult = res;
+    });
+  }
+
+  public onSelects2(files: any) {
+    this.qrcode.loadFilesToScan(files, this.config, 0.5).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      console.log(res);
+      this.qrCodeResult2 = res;
+    });
+  }
 }
+
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
+import { SafePipe } from './safe.pipe';
+
+LOAD_WASM().subscribe((res: any) => console.log('LOAD_WASM', res));
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxScannerQrcodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
